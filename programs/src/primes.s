ae0df1e430e29625f37c9a1639231ffbb4ea0a86
Robert Walsh <robert.warner.walsh@gmail.com>
2024-10-30 21:22:57 -0700

    more primes

--- programs/src/primes.s
+++ programs/src/primes.s
@@ -11,6 +11,7 @@ PSTARTHI = $EE6B
 POS       = $04 ; Current base prime we are looking at
 END       = $08 ; Last bit position we will consider
 MARK      = $0C ; Current position to mark as not prime
+PRIMES    = $0F
 
 .org START
                 jsr DISPLAY_CLEAR
@@ -21,16 +22,20 @@ MARK      = $0C ; Current position to mark as not prime
                 sta HEAP+2
                 sta HEAP+3
                 jsr DISPLAY_NUM
-                jsr ONE_SEC_DELAY
+                jsr five_secs
 
-                lda #$07
+                lda #$08
                 sta END
                 lda #$00
                 sta POS
+                sta PRIMES
 loop:
                 jsr print_prime
                 jsr mark_non_primes
                 jsr move_to_next_prime
+                jsr five_secs
+                lda POS
+                cmp #$08
                 bne loop
 stop:
                 jmp stop
@@ -55,14 +60,51 @@ get_num:
                 rts
 
 mark_non_primes:
+                ; POS is the bit of the current prime
+                ; set the bit in A
+                lda POS
+                sta MARK
+@mark_non_primes_loop
+                jsr set_bit
+                ora PRIMES
+                sta PRIMES
+                lda MARK
+                cmp END
+                bne @mark_non_primes_loop
+                rts
+
+set_bit:
+                lda #80
+                tax
+                lda MARK
+set_bit_loop:
+                beq @set_bit_done
+                asl 
+                dex
+                jmp set_bit_loop
+@set_bit_done:
                 rts
 
 move_to_next_prime:
                 inc POS
-                cmp #$07
+                cmp END
+                beq @move_to_next_prime_done
+                sta MARK
+                jsr set_bit
+                and PRIMES
+                bne move_to_next_prime
+@move_to_next_prime_done
                 rts
                 
-
+five_secs:
+                pha
+                lda #$05
+five_secs_loop:
+                jsr ONE_SEC_DELAY
+                dec
+                bne five_secs_loop
+                pla
+                rts
 
 
 
