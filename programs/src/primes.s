0eb3970014da4d9d674c8451421c4b92cc4bd200
Robert Walsh <robert.warner.walsh@gmail.com>
2025-03-23 17:14:54 -0700

    prime sieve somewhat working

--- programs/src/primes.s
+++ programs/src/primes.s
@@ -14,50 +14,39 @@ NUMBIT      = $04   ; Number of bit of current prime.  Starts at 1 (3)
 CURBIT      = $05   ; Number of bit that we are marking at not prime
 BYTE        = $06   ; Holds address of current 8 bit location of current prime 
 CURBYTE     = $08   ; Holds address of current byte we marking at not prime
-BASENUM     = $0A   ; Hold odd number value of the first bit of the current byte
-SKIP        = $0E   ; Current prime which is also how many bits to skip when
+PRIME       = $0E   ; Current prime which is also how many bits to skip when
                     ; marking not prime bits.  32 bit number
+MASK        = $12   ; Holds current bit mask
 
 ; BIT_ARRAY starts at end of program
 
 .org START
                 JSR clear_bit_array
 
-                LDA #$01
+                LDA #$00
                 STA NUMBIT
                 LDA #<BIT_ARRAY
                 STA BYTE
                 LDA #>BIT_ARRAY
                 STA BYTE+1
                 
-                LDA #$40
-                LDY #$00
-                STA (BYTE),Y
-
-                LDA #$01
-                STA BASENUM
-                LDA #$00
-                STA BASENUM+1
-                STA BASENUM+2
-                STA BASENUM+3
-
                 LDA #$02
-                STA SKIP
+                STA PRIME
                 LDA #$00
-                STA SKIP+1
-                STA SKIP+2
-                STA SKIP+3
-                LDY #$05
+                STA PRIME+1
+                STA PRIME+2
+                STA PRIME+3
+
 loop:
-                PHY
                 JSR print_prime
-                JSR calc_skip
                 JSR move_to_next_prime
+                JSR calc_prime
                 JSR mark_non_primes
-                JSR five_secs
-                PLY
-                DEY
-                BNE loop
+                LDA BYTE+1
+                CMP #$50
+                BEQ stop
+                ;JSR five_secs
+                JMP loop
 stop:
                 JMP stop
 
@@ -88,32 +77,15 @@ clear_bit_array:
 
 print_prime:
                 JSR DISPLAY_CLEAR
-                LDA SKIP
+                LDA PRIME
                 sta HEAP
-                LDA SKIP+1
+                LDA PRIME+1
                 sta HEAP+1
-                LDA SKIP+2
+                LDA PRIME+2
                 sta HEAP+2
-                LDA SKIP+3
+                LDA PRIME+3
                 sta HEAP+3
                 jsr DISPLAY_NUM
-                rts
-
-
-mark_non_primes:
-                LDA BYTE
-                STA CURBYTE
-                LDA NUMBIT
-                STA CURBIT
-@do_mark:
-                INC CURBYTE
-                BNE @inc_curbyte_done
-                INC CURBYTE+1
-@inc_curbyte_done:
-                LDA CURBYTE+1
-                CMP #$50
-                BNE @do_mark
-@set_bit_done:
                 rts
 
 move_to_next_prime:
@@ -121,67 +93,126 @@ move_to_next_prime:
                 INC NUMBIT
                 LDA NUMBIT
                 CMP #$08
-                BNE @move_to_next_prime_done
+                BNE @test_prime_bit
                 ; bit rolled over so increment the address
                 LDA #$00
                 STA NUMBIT
                 CLC
-                LDA #$10
-                ADC BASENUM
-                STA BASENUM
-                LDA #$00
-                ADC BASENUM+1
-                STA BASENUM+1
                 INC BYTE
-                BNE @move_to_next_prime_done
+                BNE @test_prime_bit
                 INC BYTE+1
-@move_to_next_prime_done:
+@test_prime_bit:
                 ; create the bit mask
                 LDA NUMBIT
-                TAX
-                LDA #$01
-@bit_mask_loop:
-                DEX
-                BEQ @test_number
-                ROL
-                jmp @bit_mask_loop
-@test_number:
-                BRK 0
+                JSR create_mask
                 LDY #$00
+                LDA MASK
                 AND (BYTE),Y
                 BNE move_to_next_prime
                 RTS
 
-calc_skip:
-                ; Start with the bit number
-                LDA NUMBIT
-                STA SKIP
+calc_prime:
+                ; Start with the byte address
+                LDA BYTE
+                STA PRIME
+                LDA BYTE+1
+                STA PRIME+1
                 LDA #$00
-                STA SKIP+1
-                STA SKIP+2
-                STA SKIP+3
-                ; Multiply by 2
+                STA PRIME+2
+                STA PRIME+3
+                ; subtract the start of the prime bits
+                SEC
+                LDA PRIME
+                SBC #<BIT_ARRAY 
+                STA PRIME
+                LDA PRIME+1
+                SBC #>BIT_ARRAY 
+                STA PRIME+1
+                ; multiple by 16 (shift 4)
+                LDY #$04
+@mult_16:       CLC
+                LDA PRIME
+                ROL
+                STA PRIME
+                LDA PRIME+1
+                ROL
+                STA PRIME+1
+                LDA PRIME+2
+                ROL
+                STA PRIME+2
+                LDA PRIME+3
+                ROL
+                STA PRIME+3
+                DEY
+                BNE @mult_16
+                ; numbits * 2 + 1
                 CLC
-                ROL SKIP
-                ROL SKIP+1
-                ROL SKIP+2
-                ROL SKIP+3
-                ; add base odd number
-                CLC
-                LDA SKIP
-                ADC BASENUM
-                STA SKIP
-                LDA SKIP+1
-                ADC BASENUM+1
-                STA SKIP+1
-                LDA SKIP+2
-                ADC BASENUM+2
-                STA SKIP+2
-                LDA SKIP+3
-                ADC BASENUM+3
-                STA SKIP+3
+                LDA NUMBIT
+                ROL
+                ADC #$01
+                ; add to prime
+                ADC PRIME
+                STA PRIME
+                LDA PRIME+1
+                ADC #$00
+                STA PRIME+1
+                LDA PRIME+2
+                ADC #$00
+                STA PRIME+2
+                LDA PRIME+3
+                ADC #$00
+                STA PRIME+3
                 RTS
                 
+mark_non_primes:
+                LDY #$00
+                LDA NUMBIT
+                STA CURBIT
+                LDA BYTE
+                STA CURBYTE
+                LDA BYTE+1
+                STA CURBYTE+1
+@do_mark:
+                ; make sure we are not at the end
+                LDA CURBYTE+1
+                CMP #$50
+                BEQ @set_bit_done
+                LDA CURBIT
+                JSR create_mask
+                LDA MASK
+                ORA (CURBYTE),Y
+                STA (CURBYTE),Y
+                CLC
+                LDA PRIME
+                AND #$07
+                ADC CURBIT
+                STA CURBIT
+                CMP #$08
+                BMI @do_mark
+                AND #$07
+                STA CURBIT
+                INC CURBYTE
+                BNE @do_mark
+                INC CURBYTE+1
+                JMP @do_mark
+@set_bit_done:
+                rts
+
+create_mask:
+                ; Create a bit mask and store it into MASK
+                ; bit number should be in A
+                TAX
+                LDA #$01
+@bit_mask_loop:
+                DEX
+                BEQ @create_mask_done
+                ROL
+                jmp @bit_mask_loop
+@create_mask_done:
+                STA MASK
+                RTS
+
+
 five_secs:
                 pha
                 lda #$05
